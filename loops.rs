fc47926ade4ada5aa6a6a42223270fa3490394b6
Maurício Moura <mauricio.moura@beteltecnologia.com.br>
2023-01-20 13:49:56 -0300

    loops

---
+++ loops.rs
@@ -0,0 +1,27 @@
+fn main () {
+    let multiplicador:u8 = 5;
+    let mut contador:u8 = 0;
+
+    while contador < 10 {
+        contador += 1;
+        if contador == 5 {
+            continue;
+        }
+        println!("{} x {} = {}", multiplicador, contador, multiplicador x contador);
+    }
+
+    contador = 0;
+    loop {
+        contador += 1;
+        println!("{} x {} = {}", multiplicador, contador, multiplicador x contador);
+        if contador == 10 {
+            break;
+        }
+    }
+    
+    //for i in 1..=10 - sinônimo      
+    for i in 1..11 { //realiza um intervalo entre 1 e 10, não chega até o último número.
+        println!("{} x {} = {}", multiplicador, i, multiplicador x i);
+    }
+
+}
