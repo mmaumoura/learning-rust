0163c682d3ac2e13d26d20c2189ebd52827f2608
mmaumoura <mmauriciosanches@hotmail.com>
2023-02-27 22:52:57 -0300

    sending first cargo

---
+++ rust-cargo/target/debug/deps/rust_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Mauricio\Documents\rust\rust-cargo\target\debug\deps\rust_cargo.exe: src\main.rs
+
+C:\Users\Mauricio\Documents\rust\rust-cargo\target\debug\deps\rust_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ rust-cargo/target/debug/rust-cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Mauricio\Documents\rust\rust-cargo\target\debug\rust-cargo.exe: C:\Users\Mauricio\Documents\rust\rust-cargo\src\main.rs
